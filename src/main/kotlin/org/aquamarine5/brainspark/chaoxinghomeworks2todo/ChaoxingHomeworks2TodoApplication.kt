2914c28f8c76a6fbab072d62102bb5e7b20f7b28
aquamarine5 <3168287806@QQ.com>
2025-03-05 22:14:32 +0800

    ax

---
+++ src/main/kotlin/org/aquamarine5/brainspark/chaoxinghomeworks2todo/ChaoxingHomeworks2TodoApplication.kt
@@ -0,0 +1,11 @@
+package org.aquamarine5.brainspark.chaoxinghomeworks2todo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChaoxingHomeworks2TodoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChaoxingHomeworks2TodoApplication>(*args)
+}
---
+++ src/test/kotlin/org/aquamarine5/brainspark/chaoxinghomeworks2todo/ChaoxingHomeworks2TodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.aquamarine5.brainspark.chaoxinghomeworks2todo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChaoxingHomeworks2TodoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
